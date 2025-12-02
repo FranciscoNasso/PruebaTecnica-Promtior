25261f8c3e36cbfff9d0e52c616a4f2f3af95ad5
FranciscoNasso <francisco.nasso@correo.ucu.edu.uy>
2025-12-02 18:34:36 -0300

    changed env for prod

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # Resolver ruta del proyecto de forma robusta
 PROJECT_ROOT = Path(__file__).resolve().parents[1]  # src/ -> parent = proyecto raíz
-ENV_PATH = PROJECT_ROOT / "environments" / "local.env"
+ENV_PATH = PROJECT_ROOT / "environments" / ".env"
 
 # Carga el .env (si existe)
 if ENV_PATH.exists():
